1b8ae5c6e79dcd0b397dc54f1895d1394bef253f
daanjvdeijk <d.j.v.d.eijk@student.tue.nl>
2021-03-10 11:52:23 +0100

    Added comments and updated requirements.txt

--- main/sprinttwo/algos_combined.py
+++ main/sprinttwo/algos_combined.py
@@ -10,7 +10,7 @@ and test database, and puts the output in a third specified database. It creates
 three dictionaries with the values from all threes the algorithms for each event and then
 adds these values to the database.
 
-Average runtime on Road_Traffic_Fines: 944.043s/15.8min
+Average runtime on Road_Traffic_Fines: >24 hours
 '''
 
 #Imports all necessary modules
@@ -33,10 +33,16 @@ class SimplePredict:
         self.data = data
         self.current_event = current_event
 
+
+    #Function that adds a temporary column to a specified file which stores all events
+    #in a trace up until a certain point. This column is removed at the end
     def addEventSeq(self, filepath):
+        #Lists used in the function
         list = []
         listAllSeq = []
 
+        #Adds events up to and including the current event to a list and appends
+        #it to a list which contains all traces. Resets when it sees ENDOFTRACE
         for x in self.data['event_concept:name']:
             if x != "ENDOFTRACE":
                 list.append(x)
@@ -45,45 +51,57 @@ class SimplePredict:
 
             listAllSeq.append(list[:])
 
+        #Writes new column to file
         self.data['eventSeq'] = listAllSeq
         self.data.to_csv(filepath, index=False)
 
+        #Returns the list with all traces for later use
         return listAllSeq
 
+    #Updated function that calculates the next event based on the current trace
     def calculate_nextevent(self, list):
+        #Lists used in the function
         selected_data = self.data[['event_concept:name']]
         all_events = []
 
+        #Looks up all next events for a certain trace and adds them to all_events
         for index, row in self.data.iterrows():
-            #print(row['eventSeq'])
             if (row['eventSeq'] == list) and index < len(self.data) - 1:
                 next_event = selected_data.at[index+1, 'event_concept:name']
                 all_events.append(next_event)
 
+        #Takes the most occuring next event
         max_event = mode(all_events)
 
-        #print(all_events)
+        #Prints which event takes place most often after the specified trace
         print(str(max_event) + " is the event that most often takes place after ")
         print(list)
 
+        #Returns both the most occuring and all next events for later use
         return max_event, all_events
 
+    #Function that calculates the chance of the predicted event to actually happen
     def calculate_nexteventchance(self, max_event, all_events):
+        #Calculates based on the list of all next events and the most often happening event
+        #how often the predicted event actually happens and converts that to rounded off percentage
         percentageOccurring = all_events.count(max_event)/len(all_events) * 100
         percentageOccurringRound = round(percentageOccurring, 2)
 
+        #Prints the result of the calculation
         print("The chance of event " + str(mode(all_events)) + " to occur next is: " + str(percentageOccurringRound) + "%")
         print(' ')
 
+        #Returns the percentage for later use
         return percentageOccurring
 
     #Second algorithm that calculates the average time between a certain event and the next one
     def calculate_avgtime(self, next_event, list):
+        #Lists used in the function
         selected_data = self.data[['event_concept:name', 'event_time:timestamp']]
         avg_time = 0
         all_time = []
 
-        #Gets the time between the current specified event and all other following events
+        #Gets the time between the current specified event trace and all other following events
         for index, row in self.data.iterrows():
             if index < len(self.data) - 1:
                 upcoming_event = selected_data.at[index + 1, 'event_concept:name']
@@ -102,65 +120,62 @@ class SimplePredict:
 
         #Calculates the average time in the set of all durations between the current specified and next event
         avg_time = sum(all_time) / len(all_time)
+        #Converts to days
         avg_time_days = avg_time/86400000
         avg_time_days_round = round(avg_time_days, 0)
 
-        #Printing statements for testing
-        #print(all_time)
-        #print("The time it takes between " + str(self.current_event) + " and " + next_event + " is on average " + str(avg_time_days_round) + " days")
+        #Prints the value
+        print("The time it takes between " + str(self.current_event) + " and " + next_event + " is on average " + str(avg_time_days_round) + " days")
 
         #Returns the value to be used later
         return avg_time_days_round
 
+    #Functions that adds a row after the end of every trace to prevent the last and first event of two
+    #different traces being compared
     def addEndOfTrace(self, filepath):
-            y = 0
-            a = 0
+        #Iterable variable being used in the for loop
+        y = 0
 
-            allCases = self.data['case_concept:name'].unique()
+        #Four lists that are used in the calculations
+        allCases = self.data['case_concept:name'].unique()
+        caseList = self.data['case_concept:name'].values.tolist()
+        eventList = self.data['event_concept:name'].values.tolist()
+        allList = self.data.values.tolist()
 
-            caseList = self.data['case_concept:name'].values.tolist()
-            eventList = self.data['event_concept:name'].values.tolist()
-            allList = self.data.values.tolist()
+        #For loop that goes over every case
+        for x in allCases:
+            #Prints progress
+            print(x)
 
-            for x in allCases:
-                #temp = next(i for i in reversed(range(len(caseEventList))) if caseEventList[i] == x)
-                print(x)
-                temp = self.lastOcurring(caseList, x, y, a)
-                eventList.insert(temp + 1, "ENDOFTRACE")
-                caseList.insert(temp + 1,"ENDOFTRACE")
-                allList.insert(temp + 1,["ENDOFTRACE","ENDOFTRACE","ENDOFTRACE","ENDOFTRACE","ENDOFTRACE"])
-                y = temp + 1
-                a += 10
+            #Location of the last occuring event is searched
+            temp = self.lastOcurring(caseList, x, y)
 
-                #traceTemp = self.data[self.data['case_concept:name'] == x].index.to_numpy()
-                #indexTemp = traceTemp[len(traceTemp) - 1] + 1
+            #ENDOFTRACE is added after the location of the last event
+            eventList.insert(temp + 1, "ENDOFTRACE")
+            caseList.insert(temp + 1,"ENDOFTRACE")
+            allList.insert(temp + 1,["ENDOFTRACE","ENDOFTRACE","ENDOFTRACE","ENDOFTRACE","ENDOFTRACE"])
 
-                #line = pd.DataFrame({"event_concept:name": "ENDOFTRACE"}, index=[indexTemp])
-                #data = pd.concat([self.data.iloc[:indexTemp], line,self.data.iloc[indexTemp:]]).reset_index(drop=True)
+            #Updates the iterable to the value after the last-added ENDOFTRACE
+            y = temp + 1
 
-            #dataTemp = pd.DataFrame(eventList, columns=['event_concept:name'])
-            #dataTemp2 = pd.DataFrame(columns=['eventID_','case_concept:name','event_lifecycle:transition','event_time:timestamp'])
-            #dataTemp3 = dataTemp.merge(self.data, how='left', left_on='event_concept:name', right_on='eventID_')
-            #print(allList)
-            self.data = pd.DataFrame(allList, columns=["eventID","case concept:name","event concept:name","event lifecycle:transition","event time:timestamp"])
-            self.data.to_csv(filepath, index=False)
-            print("Success!")
+        #Updates self.data and writes the new dataframe to the specified filepath
+        self.data = pd.DataFrame(allList, columns=["eventID","case concept:name","event concept:name","event lifecycle:transition","event time:timestamp"])
+        self.data.to_csv(filepath, index=False)
 
-    def lastOcurring(self, li, x, y, a):
-        #z = int(int(len(li)) - 0.90*int(len(li)) + a)
-        #if z > len(li):
-        #    z = int(len(li))
-        #    print("maxZ")
-
+    #Supplementary function for addEndOfTrace()
+    #Finds the index of the last event in the trace
+    def lastOcurring(self, li, x, y):
         try:
+            #Since no trace is longer than 15 events, only the next 15 events are
+            #checked. This massively reduces the runtime
             for i in reversed(range(y, y + 15)):
                 if li[i] == x:
                     return i
+        #Catches IndexErrors if the last event is not in the range, this happens for the last trace
         except IndexError:
             for i in reversed(range(y, len(li))):
                 if li[i] == x:
                     return i
-        raise ValueError("{} is not in list".format(x))
 
 def init():
     #Input for the right results:
@@ -171,23 +186,27 @@ def init():
     temp = input("Please enter a training set, a test set and a result file location: ")
     chunks = temp.split(' ')
 
-    #Opens the training database with pandas to be used in the algorithms
+    #Opens the test set with pandas
     with open(chunks[1], 'r') as file:
         data = pd.read_csv(file)
         data.columns = ((data.columns.str).replace(" ","_"))
 
+    #Adds ENDOFTRACE and the eventseq to the test set
     object = SimplePredict(data)
-    #object.addEndOfTrace(chunks[1])
+    object.addEndOfTrace(chunks[1])
     eventSeqTemp2 = object.addEventSeq(chunks[1])
 
+    #Opens the training set with pandas to be used in later algorithms
     with open(chunks[0], 'r') as file:
         data = pd.read_csv(file)
         data.columns = ((data.columns.str).replace(" ","_"))
 
+    #Adds ENDOFTRACE and the eventseq to the training set
     object = SimplePredict(data)
-    #object.addEndOfTrace(chunks[0])
-
+    object.addEndOfTrace(chunks[0])
     eventSeqTemp = object.addEventSeq(chunks[0])
+
+    #Finds all unique event sequences in the training set
     unique_data = [list(x) for x in set(tuple(x) for x in eventSeqTemp)]
 
     #Opens the test database file(chunks[1]) and creates or opens a result database file(chunks[2])
@@ -202,6 +221,7 @@ def init():
         eventdict = {('eventSeq',):'most occuring next event'}
         eventchancedict = {('eventSeq',):'chance of most occuring next event to take place'}
 
+        #Runs the algorithms for every unique event sequence and adds their result to a dictionary
         for x in unique_data:
             max_event, all_events = object.calculate_nextevent(x)
             y = object.calculate_nexteventchance(max_event, all_events)
@@ -211,27 +231,35 @@ def init():
             eventchancedict.update({tuple(x):y})
             timedict.update({tuple(x):z})
 
-        #For every row in the test file two rows are added with the new values
-        #The new database with the new rows is written into the result file
+        #Iterable variable that is used in the for loop for indexing
         i = -1
 
+        #For every row in the test file two rows are added with the new values
+        #The new database with the new rows is written into the result file
         for row in csv_reader:
+            #Skips the first row since it contains column names and not values
             if i > -1:
                 try:
                     row.append(eventdict[tuple(eventSeqTemp2[i])])
                     row.append(eventchancedict[tuple(eventSeqTemp2[i])])
                     row.append(timedict[tuple(eventSeqTemp2[i])])
+                #If there is no existing trace in the dictionary there is no information available
                 except KeyError:
                     row.append("No Information Available")
+            #Manually put in column names
             else:
                 row.append('most occuring next event in trace')
                 row.append('chance of most occuring next event taking place')
                 row.append('average time in days')
 
+            #Updates the iterable variable
             if i < len(eventSeqTemp2) - 1:
                 i += 1
 
+            #Deletes the event sequence row since it isn't supposed to be in the end result
             del row[5]
+
+            #Writes the new rows to the file
             csv_writer.writerow(row)
 
 #Runs the program
